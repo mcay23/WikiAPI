b4a707ac7ff4b7b13aad0681c072299c6f7b4fd4
mcay23 <marcelyuwono23@gmail.com>
2020-05-17 18:55:57 +0700

    removed unused imports, etc

--- src/yuwono/Page.java
+++ src/yuwono/Page.java
@@ -1,135 +1,139 @@
 package yuwono;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Iterator;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class Page {
 
-	public static final String QUERY_ID_HDR = "https://en.wikipedia.org/w/api.php?format=json&action=query"
+	public static final String QUERY_EXTRACT_HDR = "https://en.wikipedia.org/w/api.php?format=json&action=query"
 			+ "&prop=extracts&exintro&explaintext&redirects=1&pageids=";
-	public static final String QUERY_HDR = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&inprop=url&list=search&srsearch=";
-	
+	public static final String QUERY_HDR = "https://en.wikipedia.org/w/api.php"
+			+ "?action=query&format=json&list=search&srsearch=";
+	public static final String QUERY_ID_HDR = "https://en.wikipedia.org/w/api.php"
+			+ "?action=query&format=json&prop=info&inprop=url&pageids=";
+
 	private String title;
 	private String extract;
 	private String url;
 	private int id;
 	private int word_count;
-	
+
 	public Page(String url) {
-	    this.title = "";
-	    this.url = url;
-	    this.id = 0;
-	    this.extract = "";
-	    this.word_count = 0;
-	    update();
+		this.title = "";
+		this.url = url;
+		this.id = 0;
+		this.extract = "";
+		this.word_count = 0;
+		update();
 	}
-	
+
+	public static Page createPage(int id) {
+		String req = QUERY_ID_HDR + id;
+		String url = "";
+		try {
+			String response = WikiAPI.get(req);
+			JSONParser parser = new JSONParser();
+			JSONObject JSONobj = (JSONObject) parser.parse(response);
+			JSONobj = (JSONObject) JSONobj.get("query");
+			JSONobj = (JSONObject) JSONobj.get("pages");
+			JSONobj = (JSONObject) JSONobj.get(Integer.toString(id));
+			url = (String) JSONobj.get("fullurl");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Invalid Page ID createPage");
+		}
+		return new Page(url);
+	}
+
 	public void update() {
-	    if (this.url == null) {
-	        throw new NullPointerException("null url");
-	    }
-        Document doc;
-	    String url_format = "";
-	    try {
-            doc = Jsoup.connect(url).userAgent("mozilla/17.0").timeout(10000).get();
-            this.title = doc.select("#firstHeading").text();
-            url_format = this.title.replaceAll(" ", "%20");
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException("Error fetching title, invalid URL or no internet connection");
-        }
-	    
-	    String get_URL = QUERY_HDR + url_format;
-	    System.out.println(get_URL);
-	    try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(get_URL))
-                    .build();
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
-            JSONParser parser = new JSONParser();
-            JSONObject JSONwordcount = (JSONObject) parser.parse(response.body());
-            JSONObject JSONID = (JSONObject) parser.parse(response.body());
-            
-            JSONID = (JSONObject) JSONID.get("pages");
-            JSONwordcount = (JSONObject) JSONwordcount.get("query");
-            
-            JSONArray search = (JSONArray) JSONwordcount.get("search");
-            for (Object x : search) {
-            	JSONObject obj = (JSONObject) x;
-            	if (obj.get("title").equals(this.title)) {
-            		this.id = ((Long) obj.get("pageid")).intValue();
-            		this.word_count = ((Long) obj.get("wordcount")).intValue();    	
-            	}
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-	    
-	    String get_URL2 = QUERY_ID_HDR + id;
-	    try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(get_URL2))
-                    .build();
-            HttpResponse<String> response = client.send(request,
-                    HttpResponse.BodyHandlers.ofString());
-            JSONParser parser = new JSONParser();
-            JSONObject JSONextract = (JSONObject) parser.parse(response.body());
-            JSONextract = (JSONObject) JSONextract.get("query");
-            JSONextract = (JSONObject) JSONextract.get("pages");
-            JSONextract = (JSONObject) JSONextract.get(Integer.toString(id));
-            this.extract = (String) JSONextract.get("extract");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		if (this.url == null) {
+			throw new NullPointerException("null url");
+		}
+
+		Document doc;
+		String url_format = "";
+
+		try {
+			doc = Jsoup.connect(url).userAgent("mozilla/17.0").timeout(10000).get();
+			this.title = doc.select("#firstHeading").text();
+			url_format = this.title.replaceAll(" ", "%20");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Error fetching title, invalid URL or no internet connection");
+		}
+
+		String get_URL = QUERY_HDR + url_format;
+		try {
+			String response = WikiAPI.get(get_URL);
+			JSONParser parser = new JSONParser();
+			JSONObject JSONwordcount = (JSONObject) parser.parse(response);
+			JSONObject JSONID = (JSONObject) parser.parse(response);
+
+			JSONID = (JSONObject) JSONID.get("pages");
+			JSONwordcount = (JSONObject) JSONwordcount.get("query");
+			JSONArray search = (JSONArray) JSONwordcount.get("search");
+
+			for (Object x : search) {
+				JSONObject obj = (JSONObject) x;
+				if (obj.get("title").equals(this.title)) {
+					this.id = ((Long) obj.get("pageid")).intValue();
+					this.word_count = ((Long) obj.get("wordcount")).intValue();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+
+		String get_URL2 = QUERY_EXTRACT_HDR + id;
+		try {
+			String response = WikiAPI.get(get_URL2);
+			JSONParser parser = new JSONParser();
+			JSONObject JSONextract = (JSONObject) parser.parse(response);
+			JSONextract = (JSONObject) JSONextract.get("query");
+			JSONextract = (JSONObject) JSONextract.get("pages");
+			JSONextract = (JSONObject) JSONextract.get(Integer.toString(id));
+			this.extract = (String) JSONextract.get("extract");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public String getTitle() {
-	    if (title.equals("")) {
-	        return "NULL TITLE";
-	    }
+		if (title.equals("")) {
+			return "NULL TITLE";
+		}
 		return this.title;
 	}
-	
+
 	public String getURL() {
 		if (this.url.equals("")) {
 			return "NULL URL";
 		}
 		return this.url;
 	}
-	
+
 	public int getWordCount() {
-	    if (word_count == 0) {
-	        return -1;
-	    }
-	    return this.word_count;
+		if (word_count == 0) {
+			return -1;
+		}
+		return this.word_count;
 	}
-	
+
 	public String getExtract() {
-	    if (extract.equals("")) {
-	        return "NULL EXTRACT";
-	    }
-	    return this.extract;
+		if (extract.equals("")) {
+			return "NULL EXTRACT";
+		}
+		return this.extract;
 	}
-	
+
 	public int getID() {
 		if (this.id == 0) {
 			return -1;
 		}
 		return this.id;
 	}
- 
- }
+}
--- src/yuwono/WikiAPI.java
+++ src/yuwono/WikiAPI.java
@@ -1,10 +1,10 @@
 package yuwono;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -18,90 +18,102 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public abstract class WikiAPI {
-	public static final String QUERY_HDR = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=";
-	public static final String QUERY_FTR = "&format=json";
+	public static final String QUERY_HDR = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
+	public static final String QUERY_URL_HDR = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&inprop=url&titles=";
 	public static final String TOPICS_URL = "https://en.wikipedia.org/wiki/Category:Main_topic_classifications";
 	public static final String RANDOM_HDR = "https://en.wikipedia.org/wiki/Special:RandomInCategory/";
-	
-	// articles outside main page to add
-	public static final String[] more_categories = {"Good articles"};
-	
-    public static ArrayList<Page> query(String input) {
-        ArrayList<Page> pages = new ArrayList<>();
-    	String format_input = input.replaceAll(" ", "%20");
-    	String get = QUERY_HDR + format_input + QUERY_FTR;
-        try {
-	        HttpClient client = HttpClient.newHttpClient();
-	        HttpRequest request = HttpRequest.newBuilder()
-	                .uri(URI.create(get))
-	                .build();
-			HttpResponse<String> response = client.send(request,
-			        HttpResponse.BodyHandlers.ofString());
-	        JSONParser parser = new JSONParser();
-	        JSONObject jsonObject = (JSONObject) parser.parse(response.body());
+
+	// articles outside main page to
+	public static final String[] more_categories = { "Good articles" };
+
+	public static ArrayList<Page> query(String input, int threshold) {
+		ArrayList<Page> pages = new ArrayList<>();
+		String format_input = input.replaceAll(" ", "%20");
+		String request = QUERY_HDR + format_input;
+		try {
+			String response = get(request);
+			JSONParser parser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) parser.parse(response);
 			jsonObject = (JSONObject) jsonObject.get("query");
 			JSONArray search = (JSONArray) jsonObject.get("search");
-            @SuppressWarnings("unchecked")
+			@SuppressWarnings("unchecked")
 			Iterator<JSONObject> iterator = search.iterator();
-            while (iterator.hasNext()) {
-            	JSONObject page_obj = (JSONObject) iterator.next();
-            	String title = (String) page_obj.get("title");
-            	Page pg = new Page(title);
-            	pages.add(pg);
-            }
+			int i = 0;
+			while (i != threshold && iterator.hasNext()) {
+				JSONObject page_obj = (JSONObject) iterator.next();
+				int id = ((Long) page_obj.get("pageid")).intValue();
+				pages.add(Page.createPage(id));
+				i++;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-        return pages;
-    }
-    
-    public static ArrayList<String> categories() {
-        ArrayList<String> categories = new ArrayList<>();
+		return pages;
+	}
 
-        Document doc;
-        try {
-            doc = Jsoup.connect(TOPICS_URL).userAgent("mozilla/17.0").timeout(10000).get();
-            Elements topics = doc.select("div.CategoryTreeItem");
-            for (Element a : topics) {
-                String title = a.select("a").first().text();
-                categories.add(title);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        categories.remove("Main topic articles");
-        for (String x : more_categories) {
-            categories.add(x);
-        }
-        
-        for (String x : categories) {
-            System.out.print(x + "; ");
-        }
-        return categories;
-    }
-    
-    public static Page getRandomPage(String category) {
-        return getRandomPage(category, 200);
-    }
-    
-    // size_threshold is word count lower bound
-    public static Page getRandomPage(String category, int size_threshold) {
-        Response response = null;
-        Page pg;
-        do {
-            try {
-                response = Jsoup.connect(RANDOM_HDR + category).followRedirects(true).execute();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            pg = new Page(response.url().toString());
-        } while (pg.getWordCount() < size_threshold);
-        return pg;
-    }
-    
-    public static Page getPage(String url) {
-        return new Page(url);
-    }
+	public static ArrayList<Page> query(String input) {
+		return query(input, 10);
+	}
+
+	public static ArrayList<String> categories() {
+		ArrayList<String> categories = new ArrayList<>();
+
+		Document doc;
+		try {
+			doc = Jsoup.connect(TOPICS_URL).userAgent("mozilla/17.0").timeout(10000).get();
+			Elements topics = doc.select("div.CategoryTreeItem");
+			for (Element a : topics) {
+				String title = a.select("a").first().text();
+				categories.add(title);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		categories.remove("Main topic articles");
+		for (String x : more_categories) {
+			categories.add(x);
+		}
+
+		for (String x : categories) {
+			System.out.print(x + "; ");
+		}
+		return categories;
+	}
+
+	public static Page getRandomPage(String category) {
+		return getRandomPage(category, 200);
+	}
+
+	// size_threshold is word count lower bound
+	public static Page getRandomPage(String category, int size_threshold) {
+		Response response = null;
+		Page pg;
+		do {
+			try {
+				response = Jsoup.connect(RANDOM_HDR + category).followRedirects(true).execute();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			pg = new Page(response.url().toString());
+		} while (pg.getWordCount() < size_threshold);
+		return pg;
+	}
+
+	public static Page getPage(String url) {
+		return new Page(url);
+	}
+
+	public static String get(String str) throws Exception {
+		URL yahoo = new URL(str);
+		URLConnection yc = yahoo.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+		String inputLine;
+		String ret = "";
+		while ((inputLine = in.readLine()) != null)
+			ret += inputLine;
+		in.close();
+		return ret;
+	}
 }
